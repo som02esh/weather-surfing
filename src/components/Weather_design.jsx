2d93d451c0415fc6196660eb72290c658675a1d5
som02esh <106095040+som02esh@users.noreply.github.com>
2023-12-18 16:54:03 +0530

    Update Weather_design.jsx

--- src/components/Weather_design.jsx
+++ src/components/Weather_design.jsx
@@ -26,7 +26,7 @@ function Weather_design() {
   const getWeather = async () => {
     const APIKEY=import.meta.env.VITE_API_KEY;
     const location = await fetch(
-      ` http://api.openweathermap.org/geo/1.0/direct?q=${city},in&limit=2&appid=${APIKEY}`
+      ` https://api.openweathermap.org/geo/1.0/direct?q=${city},in&limit=2&appid=${APIKEY}`
     );
     const locationResult = await location.json();
     const lat = locationResult[0].lat;
